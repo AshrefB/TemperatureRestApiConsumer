e77717893b28c7202a29fdc64c2db20d075d822b
Achref Bouadila <a_bouadila@live.fr>
2020-02-26 12:35:28 +0100

    create TemperatureController and add testConsumeMin method

---
+++ src/main/java/com/ashref/rest/api/controllers/TemperatureController.java
@@ -0,0 +1,23 @@
+package com.ashref.rest.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ashref.rest.api.payload.requests.TemperatureRequest;
+import com.ashref.rest.api.payload.responses.TemperatureResponse;
+import com.ashref.rest.api.services.TemperatureService;
+
+@RestController
+@RequestMapping("/test")
+public class TemperatureController {
+	@Autowired
+	private TemperatureService service;
+	
+	@PostMapping("/min")
+	public TemperatureResponse testConsumeMin(@RequestBody TemperatureRequest request) {
+		return service.consumeMin(request);
+	}
+}
