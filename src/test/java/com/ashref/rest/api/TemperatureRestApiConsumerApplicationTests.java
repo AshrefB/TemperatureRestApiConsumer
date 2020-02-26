62058402196178fccc9bf7118d8a891a34dc42f9
Achref Bouadila <a_bouadila@live.fr>
2020-02-26 11:28:41 +0100

    first commit

---
+++ src/test/java/com/ashref/rest/api/TemperatureRestApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashref.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureRestApiConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
